f15f5f2219e034c995db26def5dd026473faa829
leesieunn <tldms32023@useb.co.kr>
2022-06-03 14:53:55 +0900

    feat: webview setting

--- app/src/main/java/com/example/kyc_kotlin_sample/MainActivity.kt
+++ app/src/main/java/com/example/kyc_kotlin_sample/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.kyc_kotlin_sample
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webview.settings.javaScriptEnabled = true
+        webview.loadUrl("https://kyc.useb.co.kr/auth")
+        webview.webViewClient = WebViewClient()
+        webview.webChromeClient = WebChromeClient()
+    }
+    override fun onBackPressed() {
+        if (webview.canGoBack())
+        {
+            webview.goBack()
+        }
+        else
+        {
+            finish()
+        }
     }
 }
