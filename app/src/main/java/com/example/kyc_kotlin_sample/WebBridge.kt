031cf28b4b5fbea957152b24f4aae60dd58f676f
leesieunn <tldms32023@useb.co.kr>
2022-06-20 02:13:40 +0900

    feat: 받는 메시지 디코딩

--- app/src/main/java/com/example/kyc_kotlin_sample/WebBridge.kt
+++ app/src/main/java/com/example/kyc_kotlin_sample/WebBridge.kt
@@ -3,6 +3,8 @@ package com.example.kyc_kotlin_sample
 import android.util.Log
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 
 
 class WebBridge {
@@ -19,6 +21,33 @@ class WebBridge {
 
     @JavascriptInterface
     fun receive(data: String) {
+
 //        Log.d("result data:", data)
+
+        val close = "{\"result\": \"close\"}"
+        val success = "{\"result\": \"success\"}"
+        val complete = "{\"result\": \"complete\"}"
+
+        val decodedData = decodedReceiveData(data)
+        if(decodedData.toString() == close)
+            Log.d("close", "KYC가 완료되지 않았습니다.")
+        else if(decodedData.toString() == success)
+            Log.d("success", "KYC 작업이 성공했습니다.")
+        else if(decodedData.toString() == complete)
+            Log.d("complete", "KYC가 완료되었습니다.")
+    }
+
+    private fun decodedReceiveData(data:String){
+
+        var decoded : String = data.fromBase64()
+        var decodedText = URLDecoder.decode(decoded, "UTF-8")
+    }
+
+    private fun String.fromBase64(): String{
+
+        return String(
+            android.util.Base64.decode(this.toByteArray(), android.util.Base64.DEFAULT),
+            StandardCharsets.UTF_8
+        )
     }
 }
